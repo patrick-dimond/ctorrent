b25e1b06d7d0392351d10ca3405d6922a5e76363
Patrick Dimond <psd@Patricks-Air.gateway>
2017-05-15 23:00:24 +1000

    Initial scaffolding continued.

--- ctorrent.c
+++ ctorrent.c
@@ -22,16 +22,54 @@
 
 #include <fcntl.h>
 
+
+extern char *optarg;
+extern int optind;
+extern int optopt;
+extern int opterr;
+extern int optreset;
+
 void usage(void) {
   
-  printf("usage: ctorrent [-d] [-p port] directory\n");
+  printf("usage: ctorrent [-d] [-p port] [-l logfile] directory\n");
   exit(1);
 
 }
 
 int main(int argc, char *argv[]) {
 
-  usage();
   
+  int daemonize = 0;
+  int port = 0;
+  int option;
+
+  if (argc < 2) {
+    usage();
+  }
+
+  while ((option = getopt(argc, argv, "dp:l:")) != -1) {
+    switch (option) {
+      case 'd':
+        daemonize = 1;
+        break;
+      case 'p':
+        /* convert optarg to a integer - check for errors and save it as the
+         * port
+         */
+        port = 1;
+        break;
+      case 'l':
+        /* Set STDOUT and STDERR to now be the logfile? */
+        break;
+      case '?':
+      default:
+        usage();
+    }
+  }
+
+  /* Check for remaining options - should just be the directory. The directory
+   * could also probably just be a default
+   */
+
   return 0;
 }
